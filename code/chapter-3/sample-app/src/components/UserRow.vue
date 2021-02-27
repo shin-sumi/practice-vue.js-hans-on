547a632fd198c23e1b20c04469a6eda356b3f60d
shin-sumi <mail@webforestwaker.com>
2021-02-27 11:21:33 +0900

    composition APIの記述を使ってロジックを書き換え

--- code/chapter-3/sample-app/src/components/UserRow.vue
+++ code/chapter-3/sample-app/src/components/UserRow.vue
@@ -16,33 +16,35 @@
 </template>
 
 <script>
-import Vue from 'vue';
+import { defineComponent, ref, nextTick } from "@vue/composition-api";
 
 export function User(nickname, email) {
   this.nickname = nickname;
   this.email = email;
 }
 
-export default Vue.extend({
+export default defineComponent({
   props: {
     user: {
       type: User,
-      required: true,
-    },
+      required: true
+    }
   },
-  data: function() {
-    return {
-      editable: false,
-    };
-  },
-  methods: {
-    edit: function() {
-      this.editable = true;
-      this.$nextTick(() => {
-        this.$refs.editNickname.focus();
+  setUp(props, context) {
+    const editable = ref(false);
+    const editNickname = ref(null);
+    const edit = () => {
+      editable.value = true;
+      nextTick(() => {
+        editNickname.value.focus();
       });
-    },
-  },
+    };
+    return {
+      editable,
+      editNickname,
+      edit
+    };
+  }
 });
 </script>
 
