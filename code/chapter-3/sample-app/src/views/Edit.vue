815a172334bbee74c1ce7ed56dc1fb5c9f8a59f7
shin-sumi <mail@webforestwaker.com>
2021-03-01 23:24:31 +0900

    composition APIをつかって画面コンポーネントを作成

--- code/chapter-3/sample-app/src/views/Edit.vue
+++ code/chapter-3/sample-app/src/views/Edit.vue
@@ -48,7 +48,64 @@
   </div>
 </template>
 
-<script></script>
+<script>
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  computed
+} from "@vue/composition-api";
+import userRowComponent, { User } from "@/components/UserRow.vue";
+
+export default defineComponent({
+  components: {
+    "user-row": userRowComponent
+  },
+  setup() {
+    const state = reactive({
+      users: [],
+      nickname: "",
+      email: "",
+      nicknameFilter: "",
+      filteredUsers: computed(() => {
+        return state.users.filter(user => {
+          user.nickname.includes(state.nicknameFilter);
+        });
+      })
+    });
+
+    const saveUser = () => {
+      const user = {
+        nickname: state.nickname,
+        email: state.email
+      };
+      state.users.push(user);
+
+      alert(
+        "ニックネーム: " +
+          state.nickname +
+          "メールアドレス" +
+          state.email +
+          "で登録しました。"
+      );
+    };
+
+    const displayUsers = () => {
+      let message = state.users.length + "人のユーザーが登録されています。";
+      for (const user of state.users) {
+        message += "\n" + user.nickname;
+      }
+      alert(message);
+    };
+
+    return {
+      ...toRefs(state),
+      saveUser,
+      displayUsers
+    };
+  }
+});
+</script>
 
 <style module lang="scss">
 .inputContainer {
