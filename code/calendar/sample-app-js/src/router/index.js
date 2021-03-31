0ea497e081149cfcb21602d7608a72ebe43d44b7
shin-sumi <mail@webforestwaker.com>
2021-03-31 22:44:42 +0900

    ナビゲーションガードとメタフィールドを利用してtitileを設定

--- code/calendar/sample-app-js/src/router/index.js
+++ code/calendar/sample-app-js/src/router/index.js
@@ -13,27 +13,42 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeComponent,
+    meta: {
+      title: 'home',
+    },
   },
   {
     path: '/calendar/:type',
     name: 'calendar',
     component: CalendarComponent,
     props: true,
+    meta: {
+      title: 'calendar',
+    },
   },
   {
     path: '/profile',
     name: 'profile',
     component: ProfileComponent,
+    meta: {
+      title: 'profile',
+    },
   },
   {
     path: '/share',
     name: 'share',
     component: ShareComponent,
+    meta: {
+      title: 'share',
+    },
   },
   {
     path: '/sign-in',
     name: 'sign-in',
     component: SignInComponent,
+    meta: {
+      title: 'sign-in',
+    },
   },
 ];
 
@@ -41,6 +56,14 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.afterEach((to) => {
+  if (!to.meta.title) {
+    return;
+  }
+
+  document.title = to.meta.title;
 });
 
 export default router;
