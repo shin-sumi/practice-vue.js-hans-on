2eeaef75b372fc355bb7d9498945a5fa1e10704a
shin-sumi <mail@webforestwaker.com>
2021-04-05 23:42:23 +0900

    Vuetifyの導入

--- code/calendar/sample-app-js/src/main.js
+++ code/calendar/sample-app-js/src/main.js
@@ -3,6 +3,9 @@ import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
 import VueCompositionAPI from '@vue/composition-api';
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 Vue.use(VueCompositionAPI);
 
@@ -11,5 +14,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
