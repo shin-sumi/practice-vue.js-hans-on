91c15d4c03a550ec03cd8bf00bc58e46d9d2c490
shin-sumi <mail@webforestwaker.com>
2021-07-11 22:25:01 +0900

    呼び出すメソッド名修正

--- code/calendar/sample-app-js/src/components/Footer.vue
+++ code/calendar/sample-app-js/src/components/Footer.vue
@@ -31,7 +31,7 @@
               v-for="(item, index) in items"
               :key="index"
               link
-              @click="handlemethod(item.methodName)"
+              @click="handleMethod(item.methodName)"
             >
               <v-list-item-icon>
                 <v-icon v-text="item.icon" />
